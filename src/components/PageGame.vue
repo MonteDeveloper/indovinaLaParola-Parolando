7bd1f0aeb66026a423b737faabb8072b14fe7b50
MonteDeveloper <lorenzomontello83@gmail.com>
2023-09-28 21:33:54 +0200

    trywords linked to squares

--- src/components/PageGame.vue
+++ src/components/PageGame.vue
@@ -1,5 +1,6 @@
 <template>
-    <div v-if="wordsLoaded" class="vh-100 d-flex flex-column justify-content-center align-items-center container-fluid gap-3">
+    <div v-if="wordsLoaded"
+        class="vh-100 d-flex flex-column justify-content-center align-items-center container-fluid gap-3">
         <!-- CHALLENGE -->
         <div v-if="isChallengeMode" class="bg-primary text-light p-5 rounded">
             <div class="fs-3 m-0">
@@ -11,39 +12,34 @@
         </div>
 
         <!-- SOLO -->
-        <div v-else class="text-light col-12 d-flex flex-column align-items-center gap-3">
+        <div v-else class="text-light col-12 d-flex flex-column justify-content-center align-items-center gap-3 flex-fill">
             <h1 class="display-1 py-3">SOLO</h1>
             <!-- QUADRATI PAROLE -->
             <div @click="alert('funziona')" class="w-100" :class="{ 'opacity-50 ': currentTry < index + 1 }"
                 v-for="(singleTry, index) in totalTry">
                 <div class="d-grid gap-1" :style="{ gridTemplateColumns: 'repeat(' + game.state.wordLength + ', 1fr)' }">
-                    <div v-for="(letter, index) in game.state.wordLength"
+                    <div v-for="i in game.state.wordLength"
                         class="d-flex align-items-center justify-content-center bg-primary rounded-1 square-box fw-bold"
                         ref="squareBox">
-                        <span class="d-flex align-items-center h-100 m-0"
-                            :class="'h' + (game.state.wordLength - 3)">A</span>
+                        <span class="d-flex align-items-center h-100 m-0" :class="'h' + (game.state.wordLength - 3)">{{
+                            tryWords[index] && tryWords[index][i - 1] ? tryWords[index][i - 1].toUpperCase() : '' }}</span>
                     </div>
                 </div>
             </div>
         </div>
 
-        <hr class="text-light rounded w-100">
-
-        <div id="keyboard" class="d-flex flex-column gap-2">
-            <div>
-                <div class="d-flex justify-content-center gap-1 mb-1" v-for="(row, rowIndex) in keyboardLayout"
+        <div id="keyboard" class="d-flex flex-column justify-content-center gap-2 h-100 w-100">
+            <div class="flex-fill d-flex flex-column justify-content-around">
+                <div class="d-flex justify-content-center gap-1 mb-1 flex-fill" v-for="(row, rowIndex) in keyboardLayout"
                     :key="'row' + rowIndex">
-                    <button class="btn btn-secondary rounded-1 col-1 d-flex align-items-center justify-content-center"
+                    <button class="btn btn-secondary rounded-1 d-flex align-items-center justify-content-center flex-fill"
                         v-for="(key, keyIndex) in row" :key="'key' + keyIndex" @click="handleKeyPress(key)">
                         {{ key.toUpperCase() }}
                     </button>
                 </div>
             </div>
-            <div class="d-flex justify-content-center gap-2">
-                <button class="btn btn-danger text-light col-5" @click="handleKeyPress('Cancella')">Cancella</button>
-                <button class="btn btn-warning text-light col-5" @click="handleKeyPress('Invio')">Invio</button>
-            </div>
         </div>
+
     </div>
 </template>
 
@@ -62,8 +58,15 @@ export default {
             keyboardLayout: [
                 ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
                 ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-                ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-            ]
+                ['CANC', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'INVIO']
+            ],
+            tryWords: [
+                'ciao',
+                'benone',
+                'bene',
+                '',
+                ''
+            ],
         };
     },
     async mounted() {
@@ -102,5 +105,9 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#keyboard {
+    height: 250px !important;
+}
+</style>
 
