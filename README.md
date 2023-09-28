7d669fd8ea641cd284758c278a66ab8089d4a15e
Lorenzo Montello <124054494+MonteDeveloper@users.noreply.github.com>
2023-09-28 11:39:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Progetto Template Personalizzato di Vite
+
+Questo è un progetto template personalizzato di Vite con già configurati Vue3, Bootstrap, FontAwesome e SCSS.
+
+## Caratteristiche
+
+- Vite per una compilazione rapida e un'esperienza di sviluppo fluida
+- Vue3 per creare interfacce utente reattive
+- Bootstrap per uno stile coerente e responsive
+- FontAwesome per icone vettoriali scalabili
+- SCSS per una gestione avanzata degli stili
+
+## Utilizzo
+
+Per utilizzare questo template, clicca sul pulsante "Use this template" per creare un nuovo repository basato su questo template. Quindi, clona il nuovo repository sul tuo computer e installa le dipendenze con `npm install`. Infine, avvia il server di sviluppo con `npm run dev` e inizia a creare il tuo progetto!
---
+++ src/components/PageMain.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+    name: "PageMain",
+    props: {
+        text: String
+    },
+    data() {
+        return {
+        }
+    },
+    mounted() {
+        console.log("Caricato il componente 'PageMain'.");
+    }
+}
+</script>
+
+<template>
+    <div class="vh-100 d-flex justify-content-center align-items-center container-fluid">
+        <div class="bg-primary text-light p-5 rounded">
+            <div class="fs-3 m-0">Questo è un template con
+                <span class="bg-warning rounded-pill p-1 px-3 text-dark">Vite</span>,
+                <span class="bg-danger rounded-pill p-1 px-3">Vue3</span>,
+                <span class="bg-warning rounded-pill p-1 px-3 text-dark">Bootstrap</span>,
+                <span class="bg-danger rounded-pill p-1 px-3">FontAwesome</span>,
+                <span class="bg-warning rounded-pill p-1 px-3 text-dark">SCSS</span>
+                già installati e configurati. Divertiti!
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
